99a4a2644184cfa2ad8af7e4d73fa6bcb1098839
Samuel Neves <sneves@dei.uc.pt>
2014-12-06 16:25:43 +0000

    Fix example

--- example.rs
+++ example.rs
@@ -7,18 +7,18 @@ pub fn main() {
     let k = [0u8, ..32];
     let n = [0u8, ..16];
     let m = "Hello, World!";
-    let c = encrypt([], m.as_bytes(), [], n, k);
+    let c = encrypt(&[], m.as_bytes(), &[], &n, &k);
     for x in c.iter() { 
         print!("{:02x} ", *x); 
     }
     println!("");
-    match decrypt([], c.as_slice(), [], n, k) {
+    match decrypt(&[], c.as_slice(), &[], &n, &k) {
         Some(m) => println!("{}", from_utf8(m.as_slice()).expect("bad utf-8")),
           None  => println!("bad ciphertext")
     }
     let mut c = c;
-    *c.get_mut(0) ^= 1; // flip a bit
-    match decrypt([], c.as_slice(), [], n, k) {
+    c[0] ^= 1; // flip a bit
+    match decrypt(&[], c.as_slice(), &[], &n, &k) {
         Some(m) => println!("{}", from_utf8(m.as_slice()).expect("bad utf-8")),
           None  => println!("bad ciphertext")
     }
