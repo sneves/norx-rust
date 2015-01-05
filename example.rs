35cbb0e2a322904973bb31dac767f4a864630f72
Michael Gehring <mg@ebfe.org>
2015-01-05 17:15:10 +0100

    Fix example

--- example.rs
+++ example.rs
@@ -4,8 +4,8 @@ use std::str::from_utf8;
 use norx::norx6441::{encrypt,decrypt};
 
 pub fn main() {
-    let k = [0u8, ..32];
-    let n = [0u8, ..16];
+    let k = [0u8; 32];
+    let n = [0u8; 16];
     let m = "Hello, World!";
     let c = encrypt(&[], m.as_bytes(), &[], &n, &k);
     for x in c.iter() { 
@@ -13,13 +13,13 @@ pub fn main() {
     }
     println!("");
     match decrypt(&[], c.as_slice(), &[], &n, &k) {
-        Some(m) => println!("{}", from_utf8(m.as_slice()).expect("bad utf-8")),
+        Some(m) => println!("{}", from_utf8(m.as_slice()).unwrap()),
           None  => println!("bad ciphertext")
     }
     let mut c = c;
     c[0] ^= 1; // flip a bit
     match decrypt(&[], c.as_slice(), &[], &n, &k) {
-        Some(m) => println!("{}", from_utf8(m.as_slice()).expect("bad utf-8")),
+        Some(m) => println!("{}", from_utf8(m.as_slice()).unwrap()),
           None  => println!("bad ciphertext")
     }
 }
